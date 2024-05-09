29f2c01881017c228f34898152ad17419202aa28
Sarthak Jain <sarthak@carefi.in>
2024-05-09 15:47:55 +0530

    Handle Errors

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,7 +8,6 @@ import classNames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
-  console.log(currentPath);
 
   const links = [
     { href: "/", label: "DashBoard" },
