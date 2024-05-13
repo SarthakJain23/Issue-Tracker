2b1ed1833e6e482c82f06843bacc6bf83ea23c44
Sarthak Jain <sarthak@carefi.in>
2024-05-13 23:31:45 +0530

    Show Delete Dialog Box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,36 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { MdDeleteOutline } from "react-icons/md";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <MdDeleteOutline />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <MdDeleteOutline />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Confirmation</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue ?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Revoke access
+            </Button>
+          </AlertDialog.Action>
+        </Flex> 
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
